702d5a157bee153b4c7cbb05ec461e429e38bdb0
Félix Morin <felixmorin@MacBook-Air-de-Felix-Morin.local>
2025-08-16 18:52:40 -0400

    update worldgen

--- src/main/java/net/kaloo234/tutorialmod/datagen/ModDataPackProvider.java
+++ src/main/java/net/kaloo234/tutorialmod/datagen/ModDataPackProvider.java
@@ -4,11 +4,15 @@ import net.kaloo234.tutorialmod.TutorialMod;
 import net.kaloo234.tutorialmod.enchantment.ModEnchantments;
 import net.kaloo234.tutorialmod.trim.ModTrimMaterials;
 import net.kaloo234.tutorialmod.trim.ModTrimPatterns;
+import net.kaloo234.tutorialmod.worldgen.ModBiomeModifiers;
+import net.kaloo234.tutorialmod.worldgen.ModConfiguredFeatures;
+import net.kaloo234.tutorialmod.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -17,7 +21,11 @@ public class ModDataPackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
             .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
-            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap);
+            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap)
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModDataPackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(TutorialMod.MOD_ID));
