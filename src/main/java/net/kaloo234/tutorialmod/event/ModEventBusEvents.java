bce927532be9a738d166a52ddad64184d9f26620
Félix Morin <felixmorin@MacBook-Air-de-Felix-Morin.local>
2025-08-19 15:34:30 -0400

    update pre-tuto 43

--- src/main/java/net/kaloo234/tutorialmod/event/ModEventBusEvents.java
+++ src/main/java/net/kaloo234/tutorialmod/event/ModEventBusEvents.java
@@ -4,10 +4,14 @@ import net.kaloo234.tutorialmod.TutorialMod;
 import net.kaloo234.tutorialmod.entity.ModEntities;
 import net.kaloo234.tutorialmod.entity.client.GeckoModel;
 import net.kaloo234.tutorialmod.entity.custom.GeckoEntity;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 
 @EventBusSubscriber(modid = TutorialMod.MOD_ID)
 public class ModEventBusEvents {
@@ -19,5 +23,12 @@ public class ModEventBusEvents {
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.GECKO.get(), GeckoEntity.createAttributes().build());
+    }
+
+    @SubscribeEvent
+    public static void registerSpawnPlacements(RegisterSpawnPlacementsEvent event) {
+        event.register(ModEntities.GECKO.get(), SpawnPlacementTypes.ON_GROUND,
+                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Animal::checkAnimalSpawnRules,
+                RegisterSpawnPlacementsEvent.Operation.REPLACE);
     }
 }
